b1582a9446592e73abc64c29ffebf15c119eecc2
Otbi18 <tan.bilgin@live.se>
2018-03-31 23:30:09 +0200

    SQL connection

---
+++ projekt/projekt/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace projekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string conString = "Data Source=TAN;Initial Catalog=projektarbete;User ID=SA;Password=***********";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
